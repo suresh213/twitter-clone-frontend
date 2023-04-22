4f14ba164ad81eb2e43cd3f63704042c185436aa
suresh213 <asuresh52012@gmail.com>
2023-04-23 03:08:41 +0530

    Add: Suspense for dynamic rendering components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import Loader from "@/components/Loader/Loader";
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
+import { Suspense } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Suspense fallback={<Loader />}>
+      <Component {...pageProps} />
+    </Suspense>
+  );
 }
