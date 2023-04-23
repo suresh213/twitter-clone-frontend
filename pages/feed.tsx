dfc9a56e378e38b8baea1e6b02a0eb1a6df884cc
suresh213 <asuresh52012@gmail.com>
2023-04-23 23:40:51 +0530

    Add: Feed page

---
+++ pages/feed.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "@/styles/Feed.module.scss";
+import Sidebar from "@/components/Feed/sidebar";
+import Home from "@/components/Feed/home/home";
+import Extras from "@/components/Feed/extras/extras";
+
+const Feed = () => {
+  return (
+    <div className={styles.feed_container}>
+      <Sidebar />
+      <Home />
+      <Extras />
+    </div>
+  );
+};
+
+export default Feed;
