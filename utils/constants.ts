994895f8879251501483eab1308ca967b9d05bfc
suresh213 <asuresh52012@gmail.com>
2023-04-23 23:33:55 +0530

    Add: Sample avatars

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,11 @@
 import twitter_logo from "../assets/logo.png";
 
 export const logo = twitter_logo;
+
+export const avatars = [
+  "https://bit.ly/ryan-florence",
+  "https://bit.ly/sage-adebayo",
+  "https://bit.ly/kent-c-dodds",
+  "https://bit.ly/prosper-baba",
+  "https://bit.ly/code-beast",
+];
