0fb02b2bf7256464784bea46ae074f6e42f69015
suresh213 <asuresh52012@gmail.com>
2023-04-26 22:36:47 +0530

    Add: Set token on refreshing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import Loader from "@/components/Loader/Loader";
+import { setToken } from "@/middleware/localStorageMiddleware";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    setToken();
+  }, []);
+
   return (
     <Suspense fallback={<Loader />}>
       <Component {...pageProps} />
