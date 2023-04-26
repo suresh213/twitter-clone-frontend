05fcfe3ab95721b06051baf6e069a1c234528126
suresh213 <asuresh52012@gmail.com>
2023-04-26 22:35:47 +0530

    Remove: Avatars

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,10 +1,3 @@
 import twitter_logo from "../assets/logo.png";
 
 export const logo = twitter_logo;
-
-export const avatars = [
-  "https://bit.ly/ryan-florence",
-  "https://bit.ly/sage-adebayo",
-  "https://bit.ly/kent-c-dodds",
-  "https://bit.ly/code-beast",
-];
