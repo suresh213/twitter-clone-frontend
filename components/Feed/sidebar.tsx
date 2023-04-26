d4a1d2fa8fba3117d7be437cebb8b2ee93125bd9
suresh213 <asuresh52012@gmail.com>
2023-04-26 22:32:13 +0530

    Refactor: Sidebar

--- components/Feed/sidebar.tsx
+++ components/Feed/sidebar.tsx
@@ -5,14 +5,18 @@ import { logo } from "@/utils/constants";
 import { BiHomeCircle, BiHash } from "react-icons/bi";
 import { RiNotification2Line } from "react-icons/ri";
 import { FiMail } from "react-icons/fi";
+import { AiOutlineLogout } from "react-icons/ai";
 import { BsBookmark } from "react-icons/bs";
 import { IoMdPerson } from "react-icons/io";
 import { TbDotsCircleHorizontal } from "react-icons/tb";
+import { Router, useRouter } from "next/router";
+import { getUser, logout } from "@/middleware/localStorageMiddleware";
 
 const navLinks = [
   {
     text: "Home",
     icon: <BiHomeCircle />,
+    path: "/feed",
   },
   {
     text: "Explore",
@@ -41,6 +45,9 @@ const navLinks = [
 ];
 
 const Sidebar = () => {
+  const router = useRouter();
+  const user = getUser();
+
   return (
     <div className={styles.sidebar}>
       <div className={styles.logo}>
@@ -48,11 +55,32 @@ const Sidebar = () => {
       </div>
       <div className={styles.navigation}>
         {navLinks.map((navLink) => (
-          <div className={styles.navigationLink} key={navLink.text}>
+          <button className={styles.navigationLink} key={navLink.text}>
             {navLink.icon}
-            <p>{navLink.text}</p>
-          </div>
+            <p
+              style={
+                router.pathname === navLink.path ? { fontWeight: 800 } : {}
+              }
+            >
+              {navLink.text}
+            </p>
+          </button>
         ))}
+
+        <button
+          className={styles.navigationLink}
+          onClick={() => logout(router)}
+        >
+          <AiOutlineLogout />
+          <p>Logout</p>
+        </button>
+      </div>
+      <div className={styles.profile}>
+        <Image src={user?.avatar} alt="logo" width={100} height={100} />
+        <div className={styles.profile_details}>
+          <p className={styles.profile_details_name}>{user?.name}</p>
+          <p className={styles.profile_details_username}>{user?.username}</p>
+        </div>
       </div>
     </div>
   );
