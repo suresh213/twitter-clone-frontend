e7e542d6298f0977e0270134a67e6fe577a8a523
suresh213 <asuresh52012@gmail.com>
2023-04-27 00:46:25 +0530

    Add: My tweets section

--- components/Feed/home/home.tsx
+++ components/Feed/home/home.tsx
@@ -6,25 +6,43 @@ import AllTweet from "./allTweets";
 import { useEffect } from "react";
 import tweetService from "@/services/tweets";
 
+const tabs = ["People", "My Tweets"];
+
 const Home = () => {
   const [tweets, setTweets] = useState<TweetsType>([]);
+  const [tab, setTab] = useState<number>(0);
 
   const getTweets = async () => {
+    const query = tab === 1 ? "?my=true" : "";
     try {
-      const res = await tweetService.getAll();
+      const res = await tweetService.getAll(query);
       setTweets(res.data.data);
     } catch (err) {}
   };
 
   useEffect(() => {
     getTweets();
-  }, []);
+  }, [tab]);
 
   return (
     <div className={styles.home}>
       <div className={styles.header}>Home</div>
       <AddTweet tweets={tweets} setTweets={setTweets} />
-      <div className={styles.tweets_header}>Tweets</div>
+      <div className={styles.tweets_header_container}>
+        {tabs.map((tabName, index) => (
+          <button
+            key={tabName}
+            onClick={() => setTab(index)}
+            className={
+              tab === index
+                ? styles.selected_tweets_header
+                : styles.tweets_header
+            }
+          >
+            {tabName}
+          </button>
+        ))}
+      </div>
       <AllTweet tweets={tweets} />
     </div>
   );
